51e16f9aed6f89b026d8d41fd7a1d44bb7afbedb
chaso-pa <130970949+chaso-pa@users.noreply.github.com>
2025-05-22 07:52:20 +0900

    LINEクライアント設定の更新

--- services/line-bot/src/config/line.ts
+++ services/line-bot/src/config/line.ts
@@ -1,14 +1,17 @@
-import { ClientConfig, MiddlewareConfig } from "@line/bot-sdk";
+import { Client, ClientConfig, MiddlewareConfig } from "@line/bot-sdk";
 import { config } from "./index.js";
 
 // LINE SDKクライアント設定
-export const lineClientConfig: ClientConfig = {
-	channelAccessToken: config.line.channelAccessToken,
-	channelSecret: config.line.channelSecret,
+export const lineConfig: ClientConfig = {
+  channelAccessToken: config.line.channelAccessToken,
+  channelSecret: config.line.channelSecret,
 };
 
 // LINE SDKミドルウェア設定
 export const lineMiddlewareConfig: MiddlewareConfig = {
-	channelAccessToken: config.line.channelAccessToken,
-	channelSecret: config.line.channelSecret,
+  channelAccessToken: config.line.channelAccessToken,
+  channelSecret: config.line.channelSecret,
 };
+
+// LINEクライアントインスタンス
+export const lineClient = new Client(lineConfig);
